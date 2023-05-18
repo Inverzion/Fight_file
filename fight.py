170b58fce2795a026f0249a054b1dcfb202f46ee
Inverzion <82619267+Inverzion@users.noreply.github.com>
2023-05-18 15:45:58 -0500

    fight.py needs grease
    Added Hero_Mistake + Potions variables to account for God Meds bug. A plan to add "Shield Health" and "Enemy_Slip"/"Hero_Slip" to the CLI Game will be up and coming soon.

--- fight.py
+++ fight.py
@@ -1,19 +1,43 @@
 import random
+import time
 
 def main():
   hero_life = 100
   enemy_life = 100
+  potions = 3
   while hero_life >= 0 and enemy_life >= 0:
-    prompt = input("How would you like to act: (attack, defend, heal)")
-    if prompt == 'attack':
-      hero_life, enemy_life = attack(hero_life, enemy_life)
-      
+    if hero_life <= 0 or enemy_life <= 0:
+      if hero_life < enemy_life:
+        print("Hero Died")
+        break
+      else:
+        print("Hero Won")
+        break
+
+    prompt = input("How would you like to act: (attack, defend, heal) ")
+
+    if hero_life <= 0 or enemy_life <= 0:
+      if hero_life < enemy_life:
+        print("Hero Died")
+        break
+      else:
+        print("Hero Won")
+        break
+
+    elif prompt == 'attack':
+      hero_life, enemy_life = hero_attack(hero_life, enemy_life)
+
     elif prompt == "defend":
-      hero_life, enemy_life = defense(hero_life, enemy_life)
+      hero_life, enemy_life = hero_defense(hero_life, enemy_life)
 
     elif prompt == "heal":
-      hero_life, enemy_life = heal(hero_life, enemy_life)
-      
+      if potions > 0:
+        hero_life, enemy_life, potions = hero_heal(hero_life, enemy_life, potions)
+      else:
+        print("You are out of potions, god speed.")
+        time.sleep(1)
+        hero_life, enemy_life = hero_mistake(hero_life, enemy_life)
+
     else:
       print("Please input a valid response")
 
@@ -25,9 +49,9 @@ def main():
         print("Hero Won")
         break
 
-def attack(enemy_life, hero_life):
+def hero_attack(hero_life, enemy_life):
+  hero_attack = random.randint(0, 100)  
   enemy_attack = random.randint(0,100)
-  hero_attack = random.randint(0, 100)
   hero_life -= enemy_attack
   enemy_life -= hero_attack
   print("Hero Attack: ", hero_attack)
@@ -36,7 +60,7 @@ def attack(enemy_life, hero_life):
   print("Enemy Health: ", enemy_life)
   return (hero_life, enemy_life)
 
-def defense(hero_life, enemy_life):
+def hero_defense(hero_life, enemy_life):
   enemy_attack = random.randint(0,100)
   hero_defend = random.randint(0, enemy_attack)
   hero_life -= (enemy_attack - hero_defend)
@@ -46,13 +70,23 @@ def defense(hero_life, enemy_life):
   print("Enemy Health: ", enemy_life)
   return (hero_life, enemy_life)
 
-def heal(hero_life, enemy_life):
+def hero_heal(hero_life, enemy_life, potions):
   enemy_attack = random.randint(0,100)
   hero_life -= enemy_attack
   hero_heal = random.randint(0, 100)
   hero_life += hero_heal
+  potions -= 1
   print("Enemy Attack: ", enemy_attack)
   print("Hero Heal: ", hero_heal)
+  print("Hero Health: ", hero_life)
+  print("Enemy Health: ", enemy_life)
+  print("Used 1 potion, ", potions, "potions left. Use these wisely!")
+  return (hero_life, enemy_life, potions)
+
+def hero_mistake(hero_life, enemy_life):
+  enemy_attack = random.randint(0,100)
+  hero_life -= enemy_attack
+  print("Enemy Attack:", enemy_attack)
   print("Hero Health: ", hero_life)
   print("Enemy Health: ", enemy_life)
   return (hero_life, enemy_life)
